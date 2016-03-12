2c2bd97aec2d1a4d2f6498858ac2b76d11a61651
Billy Kirkpatrick <wkirkpa1@fau.edu>
2016-03-12 11:18:05 -0500

    Modified initial data to have more animals

--- .Temp~/components/components.js
+++ .Temp~/components/components.js
@@ -50,7 +50,7 @@
 
 	var mod0 = _interopRequireWildcard(_import);
 
-	var _import2 = __webpack_require__(9);
+	var _import2 = __webpack_require__(5);
 
 	var mod1 = _interopRequireWildcard(_import2);
 
@@ -66,19 +66,19 @@
 
 	var mod4 = _interopRequireWildcard(_import5);
 
-	var _import6 = __webpack_require__(5);
+	var _import6 = __webpack_require__(6);
 
 	var mod5 = _interopRequireWildcard(_import6);
 
-	var _import7 = __webpack_require__(6);
+	var _import7 = __webpack_require__(7);
 
 	var mod6 = _interopRequireWildcard(_import7);
 
-	var _import8 = __webpack_require__(7);
+	var _import8 = __webpack_require__(8);
 
 	var mod7 = _interopRequireWildcard(_import8);
 
-	var _import9 = __webpack_require__(8);
+	var _import9 = __webpack_require__(9);
 
 	var mod8 = _interopRequireWildcard(_import9);
 
@@ -1180,6 +1180,205 @@
 	    value: true
 	});
 	/**
+	 * Service for Selector view components and operations.
+	 * 
+	 * @constructor
+	 * @extends wrm.core.AbstractCachedViewComponentService
+	 * @implements wrm.OperationService
+	 * @param {string} id
+	 * @param {!Object} descr
+	 * @param {!wrm.core.Manager} manager
+	 */
+	exports.default = wrm.defineService(wrm.core.AbstractCachedViewComponentService, {
+
+	    /** @override */
+	    initialize: function (descr) {
+	        var thisService = this;
+
+	        /**
+	         * @private
+	         * @type {!wrm.data.meta.Entity}
+	         */
+	        this._entity; // init'd below
+
+	        /**
+	         * @private
+	         * @type {string}
+	         */
+	        this._entityId; // init'd below
+
+	        // TODO cache query instead
+	        /**
+	         * @private
+	         * @type {!Object}
+	         */
+	        this._condExpr; // init'd below
+
+	        /**
+	         * @private
+	         * @type {!number}
+	         */
+	        this._maxResults = descr["maxResults"];
+
+	        /**
+	         * @private
+	         * @type {!boolean}
+	         */
+	        this._distinct = descr["distinct"] || false;
+
+	        /**
+	         * @private
+	         * @type {!Object}
+	         */
+	        this._output; // init'd below
+
+	        /**
+	         * @private
+	         * @type {!Object}
+	         */
+	        this._toBind; // init'd below
+
+	        /**
+	         * @private
+	         * @type {!Array}
+	         */
+	        this._order = descr["order"];
+
+	        /**
+	         * @private
+	         * @type {!wrm.data.DataService}
+	         */
+	        this._dataService; // init'd below
+
+	        return this.getManager().getDataService().then(function (dataService) {
+	            thisService._dataService = dataService;
+	            thisService._entity = dataService.getMetadata().getEntity(descr["entity"]);
+	            thisService._entityid = thisService._entity.getId();
+	            thisService._condExpr = descr["condExprs"];
+	            var output = descr["output"];
+	            thisService._output = {};
+	            thisService._toBind = {};
+	            if (output.length !== 0) {
+	                output.forEach(function (column) {
+	                    thisService._output[column["viewName"]] = column["ref"];
+	                    thisService._toBind[column["viewName"]] = column["bindName"];
+	                });
+	            } else {
+	                var keyAtt = thisService._entity.getKeyAttribute();
+	                thisService._output[keyAtt.getName()] = keyAtt.getId();
+	                thisService._toBind[keyAtt.getName()] = keyAtt.getId();
+	            }
+	        });
+	    },
+
+	    /** @override */
+	    executeOperation: function (context) {
+	        var resultsPromise = this._askResult(context.getInput());
+	        var thisService = this;
+
+	        return resultsPromise.then(function (rows) {
+
+	            var outputResult = [];
+	            var toBind = thisService._toBind;
+	            var outputLabels = thisService._output;
+
+	            rows.forEach(function (currentRow) {
+	                var outputData = {};
+	                Object.keys(outputLabels).forEach(function (key) {
+	                    outputData[toBind[key]] = currentRow[key];
+	                });
+	                outputResult.push(outputData);
+	            });
+
+	            var output = {
+	                "data": wrm.util.obj.extractPropertyValues(outputResult),
+	                "dataSize": rows.length
+	            };
+
+	            var code = output["dataSize"] === 0 ? "success.Not Found" : "success";
+	            return new wrm.nav.Output(code, output);
+	        }, function (e) {
+	            return new wrm.nav.Output("error");
+	        });
+	    },
+
+	    /** @override */
+	    createResult: function (context) {
+	        var input = context.getInput();
+	        var thisService = this;
+	        var resultsPromise = this._askResult(input);
+
+	        return resultsPromise.then(function (rows) {
+
+	            var outputResult = [];
+	            var toBind = thisService._toBind;
+	            var outputLabels = thisService._output;
+
+	            rows.forEach(function (currentRow) {
+	                var outputData = {};
+	                Object.keys(outputLabels).forEach(function (key) {
+	                    outputData[toBind[key]] = currentRow[key];
+	                });
+	                outputResult.push(outputData);
+	            });
+
+	            var output = {
+	                "data": wrm.util.obj.extractPropertyValues(outputResult),
+	                "dataSize": rows.length
+	            };
+
+	            return output;
+	        }, function (e) {
+	            thisService.getLog().error(e);
+	        });
+	    },
+
+	    /** @override */
+	    computeOutputFromResult: function (context, result) {
+	        var output = {
+	            "data": result["data"],
+	            "dataSize": result["dataSize"]
+	        };
+	        return output;
+	    },
+
+	    /**
+	     * @private
+	     * @param {!wrm.nav.Input} input
+	     * @returns {Promise|Array.<Object>}
+	     */
+	    _askResult: function (input) {
+	        var thisService = this;
+	        return this._dataService.execute(function (d) {
+	            var options = {
+	                output: thisService._output,
+	                outputConfig: { useNames: true },
+	                distinct: thisService._distinct,
+	                filter: thisService._condExpr,
+	                order: thisService._order
+	            };
+
+	            var resultsLength = input["maxResults"] || thisService._maxResults;
+	            if (resultsLength > 0) {
+	                var limit = {
+	                    count: resultsLength
+	                };
+	                options["limit"] = limit;
+	            }
+
+	            return d.select(thisService._entityid, options, input);
+	        });
+	    } });
+	module.exports = exports.default;
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	/**
 	 * Service for Create operations.
 	 * 
 	 * @constructor
@@ -1327,7 +1526,7 @@
 	module.exports = exports.default;
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	Object.defineProperty(exports, "__esModule", {
@@ -1576,7 +1775,7 @@
 	module.exports = exports.default;
 
 /***/ },
-/* 7 */
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	Object.defineProperty(exports, "__esModule", {
@@ -1628,7 +1827,7 @@
 	module.exports = exports.default;
 
 /***/ },
-/* 8 */
+/* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
 	Object.defineProperty(exports, "__esModule", {
@@ -1694,205 +1893,6 @@
 	        }, function (e) {
 	            thisService.getLog().error(e);
 	            return new wrm.nav.Output("error");
-	        });
-	    } });
-	module.exports = exports.default;
-
-/***/ },
-/* 9 */
-/***/ function(module, exports, __webpack_require__) {
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-	/**
-	 * Service for Selector view components and operations.
-	 * 
-	 * @constructor
-	 * @extends wrm.core.AbstractCachedViewComponentService
-	 * @implements wrm.OperationService
-	 * @param {string} id
-	 * @param {!Object} descr
-	 * @param {!wrm.core.Manager} manager
-	 */
-	exports.default = wrm.defineService(wrm.core.AbstractCachedViewComponentService, {
-
-	    /** @override */
-	    initialize: function (descr) {
-	        var thisService = this;
-
-	        /**
-	         * @private
-	         * @type {!wrm.data.meta.Entity}
-	         */
-	        this._entity; // init'd below
-
-	        /**
-	         * @private
-	         * @type {string}
-	         */
-	        this._entityId; // init'd below
-
-	        // TODO cache query instead
-	        /**
-	         * @private
-	         * @type {!Object}
-	         */
-	        this._condExpr; // init'd below
-
-	        /**
-	         * @private
-	         * @type {!number}
-	         */
-	        this._maxResults = descr["maxResults"];
-
-	        /**
-	         * @private
-	         * @type {!boolean}
-	         */
-	        this._distinct = descr["distinct"] || false;
-
-	        /**
-	         * @private
-	         * @type {!Object}
-	         */
-	        this._output; // init'd below
-
-	        /**
-	         * @private
-	         * @type {!Object}
-	         */
-	        this._toBind; // init'd below
-
-	        /**
-	         * @private
-	         * @type {!Array}
-	         */
-	        this._order = descr["order"];
-
-	        /**
-	         * @private
-	         * @type {!wrm.data.DataService}
-	         */
-	        this._dataService; // init'd below
-
-	        return this.getManager().getDataService().then(function (dataService) {
-	            thisService._dataService = dataService;
-	            thisService._entity = dataService.getMetadata().getEntity(descr["entity"]);
-	            thisService._entityid = thisService._entity.getId();
-	            thisService._condExpr = descr["condExprs"];
-	            var output = descr["output"];
-	            thisService._output = {};
-	            thisService._toBind = {};
-	            if (output.length !== 0) {
-	                output.forEach(function (column) {
-	                    thisService._output[column["viewName"]] = column["ref"];
-	                    thisService._toBind[column["viewName"]] = column["bindName"];
-	                });
-	            } else {
-	                var keyAtt = thisService._entity.getKeyAttribute();
-	                thisService._output[keyAtt.getName()] = keyAtt.getId();
-	                thisService._toBind[keyAtt.getName()] = keyAtt.getId();
-	            }
-	        });
-	    },
-
-	    /** @override */
-	    executeOperation: function (context) {
-	        var resultsPromise = this._askResult(context.getInput());
-	        var thisService = this;
-
-	        return resultsPromise.then(function (rows) {
-
-	            var outputResult = [];
-	            var toBind = thisService._toBind;
-	            var outputLabels = thisService._output;
-
-	            rows.forEach(function (currentRow) {
-	                var outputData = {};
-	                Object.keys(outputLabels).forEach(function (key) {
-	                    outputData[toBind[key]] = currentRow[key];
-	                });
-	                outputResult.push(outputData);
-	            });
-
-	            var output = {
-	                "data": wrm.util.obj.extractPropertyValues(outputResult),
-	                "dataSize": rows.length
-	            };
-
-	            var code = output["dataSize"] === 0 ? "success.Not Found" : "success";
-	            return new wrm.nav.Output(code, output);
-	        }, function (e) {
-	            return new wrm.nav.Output("error");
-	        });
-	    },
-
-	    /** @override */
-	    createResult: function (context) {
-	        var input = context.getInput();
-	        var thisService = this;
-	        var resultsPromise = this._askResult(input);
-
-	        return resultsPromise.then(function (rows) {
-
-	            var outputResult = [];
-	            var toBind = thisService._toBind;
-	            var outputLabels = thisService._output;
-
-	            rows.forEach(function (currentRow) {
-	                var outputData = {};
-	                Object.keys(outputLabels).forEach(function (key) {
-	                    outputData[toBind[key]] = currentRow[key];
-	                });
-	                outputResult.push(outputData);
-	            });
-
-	            var output = {
-	                "data": wrm.util.obj.extractPropertyValues(outputResult),
-	                "dataSize": rows.length
-	            };
-
-	            return output;
-	        }, function (e) {
-	            thisService.getLog().error(e);
-	        });
-	    },
-
-	    /** @override */
-	    computeOutputFromResult: function (context, result) {
-	        var output = {
-	            "data": result["data"],
-	            "dataSize": result["dataSize"]
-	        };
-	        return output;
-	    },
-
-	    /**
-	     * @private
-	     * @param {!wrm.nav.Input} input
-	     * @returns {Promise|Array.<Object>}
-	     */
-	    _askResult: function (input) {
-	        var thisService = this;
-	        return this._dataService.execute(function (d) {
-	            var options = {
-	                output: thisService._output,
-	                outputConfig: { useNames: true },
-	                distinct: thisService._distinct,
-	                filter: thisService._condExpr,
-	                order: thisService._order
-	            };
-
-	            var resultsLength = input["maxResults"] || thisService._maxResults;
-	            if (resultsLength > 0) {
-	                var limit = {
-	                    count: resultsLength
-	                };
-	                options["limit"] = limit;
-	            }
-
-	            return d.select(thisService._entityid, options, input);
 	        });
 	    } });
 	module.exports = exports.default;
